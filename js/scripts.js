87a4ec3c52d74af9711de156d27d77fe0d2746d9
Justin Lee <lee.justin001126@gmail.com>
2023-01-19 11:17:16 -0800

    add JS function that takes vowels and replaces them with dashes

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,29 @@
 // Business Logic
-function wordCounter(text) {
-  if (text.trim().length === 0) {
-    return 0;
-  }
-  let wordCount = 0;
-  const textArray = text.split(" ");
-  textArray.forEach(function(element) {
-    if (!Number(element)) {
-      wordCount++;
-    }
-  });
-  return wordCount;
-}
+function wordPuzzle(text) {
+  console.log(text)
+  let textSplit = Array.from(text);
+  console.log(textSplit)
+  for (i = 0; i <= textSplit.length - 1; i++) {
+    console.log("This for loop is being called");
+    // Checks if the current index is equal to a vowl
+    if (textSplit[i].toLowerCase() === "a" || textSplit[i].toLowerCase() === "e" || textSplit[i].toLowerCase() === "i" || textSplit[i].toLowerCase() === "o" || textSplit[i].toLowerCase() === "u") {
+      textSplit[i] = "-";
+    };
+  };
+};
 
-function numberOfOccurrencesInText(word, text) {
-  if (word.trim().length === 0) {
-    return 0;
-  }
-  const textArray = text.split(" ");
-  let wordCount = 0;
-  textArray.forEach(function(element) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++;
-    }
-  });
-  return wordCount;
-}
+
+
 // UI Logic
 
 function handleFormSubmission() {
   event.preventDefault();
-  const passage = document.getElementById("text-passage").value;
-  const word = document.getElementById("word").value;
-  const wordCount = wordCounter(passage);
-  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
-  document.getElementById("total-count").innerText = wordCount;
-  document.getElementById("selected-count").innerText = occurrencesOfWord;
-    // new lines here!
-    let boldedPassage = boldPassage(word, passage);
-    if (boldedPassage) {
-      document.querySelector("div#bolded-passage").append(boldedPassage);
-    } else {
-      document.querySelector("div#bolded-passage").innerText = null;
-    }
-}
+  const passage = document.getElementById("text-passage").value.replace(/\r\n/g,'\n');
+  // wordPuzzle(passage);
+  wordPuzzle(passage);
+};
 
-window.addEventListener("load", function() {
-  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+window.addEventListener("load", function(){
+  document.querySelector("form#word-scrambler").addEventListener("submit", handleFormSubmission);
 });
 
-function boldPassage(word, text) {
-  if ((text.trim().length === 0) || (word.trim().length === 0)) {
-    return null;
-  }
-  const p = document.createElement("p");
-  let textArray = text.split(" ");
-  textArray.forEach(function(element, index) {
-    if (word === element) {
-      const bold = document.createElement("strong");
-      bold.append(element);
-      p.append(bold);
-    } else {
-      p.append(element);
-    }
-    if (index !== (textArray.length - 1)) {
-      p.append(" ");
-    }
-  });
-  return p;
-}
